9d45b865c76495f28bdd5dd27e3aedb073e7fac6
Capra Irata <113552738+capra-irata@users.noreply.github.com>
2023-01-18 09:31:28 -0800

    Add hover event for cells, clean up CSS in JS file
    Cells change color when hovered. Inline CSS for columns and cells moved
    from etch-a-sketch.js to styles.css. Use classList.add instead for
    cleaner code.

--- etch-a-sketch.js
+++ etch-a-sketch.js
@@ -1,13 +1,19 @@
+function sketch(e) {
+  // When the cell is hovered, change its background color
+  e.target.classList.add(`hovered`);
+}
+
 function createGrid(size) {
   const sketchpad = document.querySelector(`#sketchpad`);
 
   for (i = 0; i < size; i++) {
     const col = document.createElement(`div`);
-    col.style.cssText = `display: flex; flex-direction: column; flex: 1 0 auto;`;
+    col.classList.add(`column`); // Make columns flex containers
 
     for (j = 0; j < size; j++) {
       const cell = document.createElement(`div`);
-      cell.style.cssText = `flex: 1 0 auto; border: 2px solid black;`;
+      cell.classList.add(`cell`); // Flex cells to fill column
+      cell.addEventListener(`mouseover`, sketch);
       col.appendChild(cell);
     }
 
